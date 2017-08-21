a794c97135dabb6bd156ad6f5eb441ebd6dbfd45
KARAN BHATIA <kbhatia1996@gmail.com>
2017-08-21 17:21:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,10 @@
 console.log('Loaded!');
-var img =document.getElementById('k');
+var img = document.getElementById('k');
 var marginLeft=0;
 function moveRight(){
-    marginLeft=marginLeft +50;
+    marginLeft = marginLeft + 50;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onClick=function(){
-    var interval= setInterval(moveRight,50);
+    var interval= setInterval(moveRight,100);
 };
